0afff1e4028a6264fdc4651ffb84f543d4f4b8a7
digiDreams <digital2dreamer@gmail.com>
2022-06-16 14:59:23 +0200

    sylvainView

---
+++ XcodeAndYou/sylvainView.swift
@@ -0,0 +1,20 @@
+//
+//  sylvainView.swift
+//  XcodeAndYou
+//
+//  Created by apprenant55 on 16/06/2022.
+//
+
+import SwiftUI
+
+struct sylvainView: View {
+    var body: some View {
+        Text("Sylvain")
+    }
+}
+
+struct sylvainView_Previews: PreviewProvider {
+    static var previews: some View {
+        sylvainView()
+    }
+}
