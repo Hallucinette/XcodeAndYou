54671ce61afcaa422782327ba23607f1a6dd492b
Amelie Pocchiolo <ameliepocchiolo@gmail.com>
2022-06-16 13:55:48 +0200

    Initial Commit

---
+++ XcodeAndYou/XcodeAndYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeAndYouApp.swift
+//  XcodeAndYou
+//
+//  Created by apprenant59 on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct XcodeAndYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
