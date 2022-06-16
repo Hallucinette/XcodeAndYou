ffcdf957e29454e10b660cde692b1408ae6accc2
Solobolo <contact.marie.css@gmail.com>
2022-06-16 05:14:18 -0700

    add marie page

---
+++ XcodeAndYou/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  XcodeAndYou
+//
+//  Created by Apprenant 11 on 16/06/2022.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Marie was here!")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
