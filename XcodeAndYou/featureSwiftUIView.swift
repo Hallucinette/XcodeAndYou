0ba65499d967ba1faa5f062622ffbef3a63bc3fc
pdgswift <paridenis1@gmail.com>
2022-06-16 14:23:34 +0200

    add new file via Xcode

---
+++ XcodeAndYou/featureSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  featureSwiftUIView.swift
+//  XcodeAndYou
+//
+//  Created by Apprenant 91 on 16/06/2022.
+//
+
+import SwiftUI
+
+struct featureSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct featureSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        featureSwiftUIView()
+    }
+}
